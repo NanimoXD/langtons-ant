8965fa000805754e650c208f0c031f36e67a92f8
ciezczakmichal <ciezczakmichal@users.noreply.github.com>
2017-01-22 13:28:28 +0100

    Usunięcie klasy sprite-u mrówki

--- Ant.cpp
+++ Ant.cpp
@@ -1,25 +1,16 @@
 #include "Ant.hpp"
 
+
 Ant::Ant()
     : direction(Direction::Up)
 { }
 
-Ant::~Ant()
-{ }
-
 void Ant::rotateAnt(Direction dir)
 {
-    Direction prev_dir = direction;
     direction = rotateDirection(direction, dir);
-    directionChanged(prev_dir);
 }
 
 void Ant::setDirection(Direction dir)
 {
-    Direction prev_dir = direction;
     direction = dir;
-    directionChanged(prev_dir);
 }
-
-void Ant::directionChanged(Direction /*previous_direction*/)
-{ }
--- Ant.hpp
+++ Ant.hpp
@@ -5,15 +5,13 @@
 #include <SFML/System/Vector2.hpp>
 
 
-/* Definicja mrówki bez graficznej reprezentacji */
+/* Definicja mrówki w symulacji */
 
 class Ant
 {
 public:
     /* Konstruuje mrówkę skierowaną do góry, w pozycji (0, 0) */
     Ant();
-
-    virtual ~Ant();
 
     /* Zwraca absolutny kierunek */
     inline Direction currentDirection() const;
@@ -27,10 +25,6 @@ public:
     /* Pozycja mrówki na planszy (informacja dla obiektu klasy Board) */
     void setPositionOnBoard(const sf::Vector2u &pos);
     inline const sf::Vector2u & positionOnBoard() const;
-
-protected:
-    /* Informacja dla klas potomnych, że kierunek się zmienił */
-    virtual void directionChanged(Direction previous_direction);
 
 private:
     Direction direction;
